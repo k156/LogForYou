1b9beedd7e6d13cc82b7352bb8303579d45558cb
k156 <jm_91@live.co.kr>
2019-03-28 18:04:46 +0900

    dd

--- helloflask/__init__.py
+++ helloflask/__init__.py
@@ -2,10 +2,24 @@ from flask import Flask, g, Response, make_response, request, render_template, M
 from datetime import date, datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from helloflask.templates.init_db import init_database, db_session
+from flask import url_for
 
 
 app = Flask(__name__)
 app.debug = True  
 
 
-@app.route('/res1')
+@app.route('/')
+def main():
+    return render_template("application.html")
+
+
+@app.route('/sign_in')
+def sign_in():
+    return render_template("form.html")
+
+
+    
+@app.route('/test')
+def test():
+    return render_template("main.html")

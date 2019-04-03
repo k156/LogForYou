8aaa7c746361c2ca11705f4789f64505d53f03c2
k156 <jm_91@live.co.kr>
2019-04-03 18:08:01 +0900

    application, sign_up, main

--- helloflask/__init__.py
+++ helloflask/__init__.py
@@ -1,22 +1,54 @@
-from flask import Flask, g, Response, make_response, request, render_template, Markup
+from flask import Flask, g, Response, make_response, request, render_template, Markup, session, redirect, flash
 from datetime import date, datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from helloflask.templates.init_db import init_database, db_session
+from helloflask.models import Patient, Doctor
 from flask import url_for
 
 
 app = Flask(__name__)
 app.debug = True  
 
+app.config.update(
+	SECRET_KEY='X1243yRH!mMwf',
+	SESSION_COOKIE_NAME='pyweb_flask_session',
+	PERMANENT_SESSION_LIFETIME=timedelta(31)      # 31 days
+)
 
 @app.route('/')
 def main():
     return render_template("main.html")
 
+@app.route('/sign_in', methods=['GET'])
+def show_sign_in():
+    return render_template("form_extended.html")
 
-@app.route('/sign_in')
+@app.route('/sign_in', methods=['POST'])
 def sign_in():
-    return render_template("form.html")
+    email = request.form.get('email')
+    passwd = request.form.get('passwd')
+    table = request.form.get('table')
+
+    if table == 'patient':
+        u = Patient.query.filter('email = :email and password = sha2(:passwd, 256)').params(email=email, passwd=passwd).first()
+    else:
+        u = Doctor.query.filter('email = :email and password = sha2(:passwd, 256)').params(email=email, passwd=passwd).first()
+
+    if u is not None:
+        session['loginUser'] = { 'userid': u.id, 'name': u.name }
+        if session.get('next'):
+            next = session.get('next')
+            del session['next']
+            return redirect(next)
+        return render_template("main.html")
+        # return redirect('/')
+    else:
+        flash("해당 사용자가 없습니다!!")
+        return render_template("form_extended.html")
+        
 
 
+@app.route('/sign_up')
+def sign_up():
+    return render_template("sign_up.html")
     
--- helloflask/templates/init_db.py
+++ helloflask/templates/init_db.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Declare connection
-mysql_url = "mysql+pymysql://root:1234567@35.243.74.84/gcp_melondb?charset=utf8"
+# mysql_url = "mysql+pymysql://root:1234567@35.243.74.84/gcp_melondb?charset=utf8"
+mysql_url = "mysql+pymysql://root:log190321@35.194.112.136/logforyoudb?charset=utf8"
 engine = create_engine(mysql_url, echo=True, convert_unicode=True)
 # Declare & create Session
 db_session = scoped_session( sessionmaker(autocommit=False, autoflush=False, bind=engine) )

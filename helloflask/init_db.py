e4e082ced73c36d2c50b3c6472ad311094fd2e6d
k156 <jm_91@live.co.kr>
2019-04-22 10:59:27 +0900

    keys removed

--- helloflask/init_db.py
+++ helloflask/init_db.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+from keys import mysql_url
 
 # Declare connection
-# mysql_url = "mysql+pymysql://root:1234567@35.243.74.84/gcp_melondb?charset=utf8"
-mysql_url = "mysql+pymysql://root:log190321@35.194.112.136/logforyoudb?charset=utf8"
-engine = create_engine(mysql_url, echo=True, convert_unicode=True)
+mysql = mysql_url
+engine = create_engine(mysql, echo=True, convert_unicode=True)
 
 # Declare & create Session
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine) )
